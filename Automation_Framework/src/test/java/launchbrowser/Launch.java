f0fb24c5dede41aa09352e5c16886757597e6532
ganes <ganes@DESKTOP-8AKJV26>
2025-07-01 13:16:38 +0530

    Added Empty Web Browser

---
+++ Automation_Framework/src/test/java/launchbrowser/Launch.java
@@ -0,0 +1,18 @@
+package launchbrowser;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+
+	@Test
+	public void LaunchEmptyBrowser() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com/");
+	}
+}
